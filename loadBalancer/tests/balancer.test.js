b0fc92581a7a823bd50dbaf470235c298701a3c6
Jacer101 <46888542+Jacer101@users.noreply.github.com>
2025-09-23 12:09:16 -0500

    Update balancer.test.js

--- loadBalancer/tests/balancer.test.js
+++ loadBalancer/tests/balancer.test.js
@@ -3,7 +3,7 @@ const request = require("supertest");
 
 describe("Load Balancer Response Test", () => {
   test("should respond to GET request", async () => {
-    const response = await request("http://localhost:8080").get("/tests");
+    const response = await request("http://localhost:8080").get("/health");
     expect(response.status).toBe(200);
   });
 });
